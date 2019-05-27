9f83a503691178acf33da3e819416d6a8732a61a
cjswo7119 <cjswo7119@gmail.com>
2019-05-27 16:51:25 +0900

    조인추가

--- app/src/main/java/user/inhatc/myshell/MainActivity.java
+++ app/src/main/java/user/inhatc/myshell/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -11,7 +12,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
 
         Button btn2 = (Button)findViewById(R.id.button);
         btn2.setOnClickListener(this);
@@ -19,7 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        Intent intent = new Intent(MainActivity.this, JoinActivity.class);
         startActivity(intent);
     }
 }
